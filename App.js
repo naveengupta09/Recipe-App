f4eee1b6e1863a70db9f56f21dc230d9c83f7d92
Naveen09 <naveenkumar54111@gmail.com>
2026-01-10 00:11:09 +0530

    today i add next step

---
+++ App.js
@@ -0,0 +1,7 @@
+const searchBox = document.querySelector('.searchBox');
+const searchBtn= document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
+
+searchBtn.addEventListener('click' , () => {
+    
+});
